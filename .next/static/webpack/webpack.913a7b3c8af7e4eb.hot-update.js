c1eb1ea0baac5a1f8b8ae03cee8c20e4be66a900
MariAntonia-010 <mariantonia.alves@rede.ulbra.br>
2024-11-24 16:42:10 -0300

    atualização da biblioteca

--- .next/static/webpack/webpack.913a7b3c8af7e4eb.hot-update.js
+++ .next/static/webpack/webpack.913a7b3c8af7e4eb.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ (() => {
-/******/ 	__webpack_require__.h = () => ("b01d0001688bee07")
+/******/ 	__webpack_require__.h = () => ("cc7343e9afa1efb6")
 /******/ })();
 /******/ 
 /******/ }
